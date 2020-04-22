bab049f4fa41ab808f039f0cb2e8f3a52d935540
renatodln123 <renato.brtkm@gmail.com>
2020-04-22 13:21:32 -0300

    inserir e atualizar no banco

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 Route::get('/', function () {
+// Reculperar dados do banco de dados 
+
     // $sql = 'SELECT * FROM users WHERE id = 4';
     // $users = \DB::select($sql);
 
@@ -23,5 +25,20 @@ Route::get('/', function () {
     //                 ->select('id','name')
     //                 ->get();
     // dd($users);
+
+   // inserir dados no banco de dados
+   
+   //$user = new \App\User;
+
+   // é utilizado os metodos mágicos __set e __get pra entender que é cada coluna no banco
+   $user = \App\User::find(31);
+   $user->name = 'renatinho';
+   $user->email = 'renato.brtkm@gmail.com';
+   $user->password = bcrypt('9090tkm123');
+   $user->save();
+
+    
     return view('welcome');
+
+
 });
